bfc0705ee44045897e77f59f57bcaf7e434cc585
Sanjay Rawat <sanjayrawat0570@gmail.com>
2025-07-25 10:40:09 +0530

    sup

--- app/api/incidents/route.ts
+++ app/api/incidents/route.ts
@@ -1,17 +1,22 @@
-import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
+import { prisma } from "../../../lib/prisma";
 
 export async function GET(req: Request) {
-  const url = new URL(req.url);
-  const resolved = url.searchParams.get("resolved") === "true";
+  try {
+    const url = new URL(req.url);
+    const resolved = url.searchParams.get("resolved") === "true";
 
-  const incidents = await prisma.incident.findMany({
-    where: { resolved },
-    include: { camera: true },
-    orderBy: { tsStart: "desc" },
-  });
-  console.log(incidents)
+    const incidents = await prisma.incident.findMany({
+      where: { resolved },
+      include: { camera: true },
+      orderBy: { tsStart: "desc" },
+    });
 
-  return NextResponse.json(incidents);
+    return NextResponse.json(incidents);
+  } catch (err) {
+    console.error("❌ API ERROR /api/incidents", err);
+    return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+  }
 }
+
 
